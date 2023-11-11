06db035b235d1181d8a1af1237f2f40838ec807e
1Akane <149177553+1Akane@users.noreply.github.com>
2023-11-11 15:18:15 -0500

    Update config.py

--- zeenode/zeenode/config.py
+++ zeenode/zeenode/config.py
@@ -1,5 +1,5 @@
 class config:
-    global auth; auth = "" # Enter your discord token for Auto-Login.
-    global prefix; prefix = "$" # Enter your prefix for selfbot.
+    global auth; auth = "ODUwMTc2MzExOTUxNjIyMTk1.GhqNSs.b9uB3A1ZxNrSh5Y6bG_6uRJeJDjjKbhzyyhe2Q" # Enter your discord token for Auto-Login.
+    global prefix; prefix = "*" # Enter your prefix for selfbot.
     global nitro_sniper; nitro_sniper = "true" # 'true' to enable nitro sniper, 'false' to disable.
     global giveaway_sniper; giveaway_sniper = "true" # 'true' to enable giveaway sniper, 'false' to disable.
